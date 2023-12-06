a8c656899ca0309117f44ae8dc3fafbbf5945d21
ahimeir <a.benisti88@gmail.com>
2023-12-06 03:06:18 +0200

    .

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/calculate', async (req, res) => {
   try {
-    const response = await axios.post('http://localhost:3001/calculate', req.body);
+    // Replace 'localhost' with the service name or container name of your calculation service.
+    // For example, if your calculation service container's name is 'calculation-service', use:
+    const response = await axios.post('http://mortgage-server-container:3001/calculate', req.body);
     res.send(`Monthly Payment: ${response.data.monthlyPayment}`);
   } catch (error) {
+    console.error('Error: ', error.message);  // Log the error for debugging
     res.status(500).send('Error in calculation service');
   }
 });
